00f8c96cb1b5df0b7b396f5ce657a1622cfe5557
EliamJMC <jozadad2007@gmail.com>
2026-01-17 21:03:36 -0500

    2026-01-17

--- ConsolUI/Main.cpp
+++ ConsolUI/Main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <iomanip>
 #include <conio.h>
 #include <cstdlib>
 #include <string>
 #include <functional>
 #include <vector>
-#include <initializer_list>
 #include <map>
 #include <stack>
 #include <memory>
 #include <optional>
 #include <algorithm>
+#include <utility>
 
 #include "db.h"
 #include "cvm 25.h"
@@ -26,8 +25,9 @@ struct Size { int height, width; };
 class Component;
 class Conteiner;
 class Label;
-class Selector;
-class Page;
+class Menu;
+class TextBox;
+class Window;
 class Catalog;
 
 //---------------------------//
@@ -39,7 +39,7 @@ protected:
     Component* parent = nullptr;
 public:
     Position relative_Position;
-    Size size; 
+    Size size;
 
     Component(Position p, Size s)
         : relative_Position(p), size(s) {
@@ -56,6 +56,9 @@ public:
     }
 
     virtual void render() = 0;
+
+    virtual bool handle_Input(char key) { return false; }
+
     void setParent(Component* par) { parent = par; }
 };
 
@@ -71,18 +74,44 @@ public:
 
     /*
     * Sets parent pointer and adds child unique pointer.
-    * @param child Any Object That Heritates Form Component
+    * @param child Any object that heritates form Component
     */
     void addChild(unique_ptr<Component>&& child) {
         child->setParent(this);
         children.push_back(move(child));
     }
 
-    /* Renders every child component on screen.*/
-    virtual void render() override {
-        for (const auto& child : children) child->render();
+    /* Finds all Components of the same class.*/
+    template<typename Type>
+    vector<Type*> findByType() {
+        vector<Type*> filtered;
+        for (const auto& child : children) {
+            if (auto pointer = dynamic_cast<Type*>(child.get()))
+                filtered.push_back(pointer);
+        }
+        return filtered;
     }
 
+    /* Finds the first Component of a specific class */
+    template<typename Type>
+    Type* findFirstByType() {
+        if (children.empty()) return nullptr;
+        for (const auto& child : children) {
+            if (auto pointer = dynamic_cast<Type*>(child.get()))
+                return pointer;
+        }
+        return nullptr;
+    }
+
+    /* Renders every child component on screen.*/
+    virtual void render() override {
+        for (const auto& child : children)
+            child->render();
+    }
+
+    vector<unique_ptr<Component>>& getChildren() {
+        return children;
+    }
 private:
     vector<unique_ptr<Component>> children;
 };
@@ -98,8 +127,8 @@ public:
     }
     enum Orientation { Vertical, Horizontal };
 
-    /* 
-    * Set the color that will be printed on screen. 
+    /*
+    * Set the color that will be printed on screen.
     * @param color Color that will be set.
     */
     void setColor(Color _color) { color = _color; }
@@ -111,7 +140,7 @@ public:
         Position position_Absolute = absolute_Position();
         string output = text;
 
-        if (spaced && !text.empty()) {
+        if (spaced() && !text.empty()) {
             output.clear();
             for (size_t i = 0; i < text.length(); i++) {
                 output.push_back(text[i]);
@@ -148,8 +177,8 @@ public:
     * @param _spaced There will be a _spacement number of spaces between the letters.
     * @param _spacement The amount of spaces between the letters.
     */
-    void setMode(bool _centered = false, bool _spaced = false, int _spacement = 0) {
-        centered = _centered; spaced = _spaced; spacement = _spacement;
+    void setMode(bool _centered = false, int _spacement = 0) {
+        centered = _centered; spacement = _spacement;
     }
 
     /*
@@ -168,8 +197,11 @@ private:
     Color color = Color::wht;
 
     bool centered = false;
-    bool spaced = false;
     int spacement = 0;
+    bool spaced() const {
+        if (spacement > 0) return true;
+        return false;
+    }
 };
 
 //---------------------//
@@ -177,9 +209,10 @@ private:
 //---------------------//
 
 class TextBox : public Component {
-public: 
+public:
     TextBox(Position p = { 0,0 }, Size s = { 3,7 }, Action act = nullptr)
-        : Component(p, s), action(act) {}
+        : Component(p, s), action(act) {
+    }
 
     virtual void render() override {
         for (int y = 0; y < size.height; ++y) {
@@ -193,21 +226,24 @@ public:
         }
     }
 
-    void handle_Input() {
-        optional<char> ch = nullopt;
-        gotoxy(static_cast<size_t>(absolute_Position().x + 1), static_cast<size_t>(absolute_Position().y + size.height / 2));
-        while (ch != 13) {
-            ch = verify_character();
+    virtual bool handle_Input(char key) override {
+        handle_Text_Input(static_cast<optional<char>>(key));
+        return true;
+    }
 
-            if (ch == 8) {
-                if (!word().empty()) {
-                    word().pop_back();
+    void handle_Text_Input(optional<char> key) {
+        gotoxy(static_cast<size_t>(absolute_Position().x + 1), static_cast<size_t>(absolute_Position().y + size.height / 2));
+        while (key != 13) {
+            key = verify_character();
+            if (key == 8) {
+                if (!word.empty()) {
+                    word.pop_back();
                     cout << " \b";
                 }
             }
-            else if (ch.has_value()) {
-                word().push_back(ch.value());
-                cout << ch.value(); 
+            else if (key.has_value() && word.length() <= static_cast<size_t>(size.width) - 2) {
+                word.push_back(key.value());
+                cout << key.value();
             }
         }
     }
@@ -219,35 +255,31 @@ private:
     }
 
     Action action;
-    string word() {
-        if (word().length() > size.width - 2)
-            return word().substr(0, size.width - 2);
-        else return word();
-    }
+    string word;
 };
 
-//----------------------//
-//-- CLASS : SELECTOR --//
-//----------------------//
+//------------------//
+//-- CLASS : MENU --//
+//------------------//
 
-class Selector : public Component {
+class Menu : public Component {
 public:
 
-    Selector(Position p = { 0,0 }, Size s = { 30,120 })
+    Menu(Position p = { 0,0 }, Size s = { 30,120 })
         : Component(p, s) {
     }
 
     /* Renders every child (Label) component on screen.*/
-    void render() override {
+    virtual void render() override {
         for (auto& item : items)
             item.label->render();
     }
 
-    /* 
-    * Switch between the Labels in the selector.
+    /*
+    * Switch between the Labels in the menu.
     * @param key The letter that set the direction.
     */
-    void switch_Selection(char key) {
+    void switch_selection(char key) {
         if (items.empty()) return;
 
         if (key == 13) {
@@ -256,9 +288,9 @@ public:
             return;
         }
 
-        int dir = 
+        int dir =
             (key == 'w') ? -1 :
-            (key == 's') ? 1 : 
+            (key == 's') ? 1 :
             0;
 
         if (dir == 0) return;
@@ -285,10 +317,15 @@ public:
         items.emplace_back(label, next, back ? optional<Action>(back) : nullopt);
         label->setParent(this);
 
-        if (items.size() == 1) 
+        if (items.size() == 1)
             items[0].label->setColor(Color::gry);
     }
-      
+
+    virtual bool handle_Input(char key) override {
+        switch_selection(key);
+        return true;
+    }
+
 private:
     struct Item {
         Label* label;
@@ -300,30 +337,47 @@ private:
     size_t selectedIndex = 0;
 };
 
-//------------------//
-//-- CLASS : PAGE --//
-//------------------//
+//--------------------//
+//-- CLASS : WINDOW --//
+//--------------------//
 
-class Page {
+class Window {
 protected:
     string name;
-    Selector* selector = nullptr;
-
 public:
-    Page(string _name)
-        : root({ 0,0 }, { 30,120 }), name(_name) {}
+    Window(string _name)
+        : root({ 0,0 }, { 30,120 }), name(_name) {
+    }
 
     Conteiner root;
-    bool open = false;
 
-    /* Set the Selector. */
-    void set_Selector(Selector* sel) { selector = sel; }
+    bool open = false;
 
     void handle_Input(char key, Catalog& catalog);
 
-    void read_Page() {
-        root.render();
+    void open_Window() { root.render(); }
+
+
+
+private:
+    Component* first_Focus() const {
+        if (auto conteiner = const_cast<Conteiner&>(root).findFirstByType<Conteiner>()) {
+            for (auto& child : conteiner->getChildren())
+                if (auto menu = dynamic_cast<Menu*>(child.get()))
+                    return menu;
+                else if (auto textBox = dynamic_cast<TextBox*>(child.get()))
+                    return textBox;
+        }
+        else if (auto menu = const_cast<Conteiner&>(root).findFirstByType<Menu>())
+            return menu;
+        else if (auto textBox = const_cast<Conteiner&>(root).findFirstByType<TextBox>())
+            return textBox;
+        else
+            return nullptr;
     }
+    Component* focus = first_Focus();
+    stack<Component*> focus_Stack;
+
 };
 
 //---------------------//
@@ -333,51 +387,58 @@ public:
 class Catalog {
 public:
     void run() {
-        redraw();
-        while (!pageStack.empty()) {
+        update();
+        while (!windowStack.empty()) {
             char key = _getch();
             current()->handle_Input(key, *this);
         }
     }
 
-    void push(Page* page) {
-        if (!pageStack.empty())
-            pageStack.top()->open = false;
+    void push(unique_ptr<Window> window) {
+        if (!windowStack.empty())
+            windowStack.top()->open = false;
 
-        page->open = true;
-        pageStack.push(page);
-        redraw();
+        window->open = true;
+        windowStack.push(move(window));
+        update();
     }
 
     void pop() {
-        if (pageStack.size() <= 1) {
+        if (windowStack.size() <= 1) {
             exit(0);
         }
 
-        pageStack.top()->open = false;
-        pageStack.pop();
-        pageStack.top()->open = true;
-        redraw();
+        windowStack.top()->open = false;
+        windowStack.pop();
+        windowStack.top()->open = true;
+        update();
     }
 
-    Page* current() {
-        return pageStack.empty() ? nullptr : pageStack.top();
+    Window* current() {
+        return windowStack.empty() ? nullptr : windowStack.top().get();
     }
 
 private:
-    void redraw() {
+    void update() {
         clrscr();
-        if (current()) current()->read_Page();
+        if (current())
+            current()->open_Window();
     }
 
-    stack<Page*> pageStack;
+    stack<unique_ptr<Window>> windowStack;
 };
 
-// Implementation del metodo de page
-void Page::handle_Input(char key, Catalog& catalog) {
-    if (key == 27) { catalog.pop(); return; }
-    if (selector) selector->switch_Selection(key);
+// Implementation del metodo de window
+void Window::handle_Input(char key, Catalog& catalog) {
+    if (!open) return;
+    if (key == 27) {
+        catalog.pop();
+        return;
+    }
+
+    if (focus) focus->handle_Input(key);
 }
+
 
 //------------------------------//
 //-- MAIN : PROGRAM EXECUTION --//
@@ -386,40 +447,41 @@ void Page::handle_Input(char key, Catalog& catalog) {
 int main() {
     Catalog catalog;
 
-    Page main_Page("Main Page");
-    Page start_Page("Start Page");
-    Page options_Page("Options Page");
+    auto main_Window = make_unique<Window>("Main Window");
+    auto start_Window = make_unique<Window>("Start Window");
+    auto options_Window = make_unique<Window>("Options Window");
 
     // ===== MAIN PAGE =====
-    auto title = make_unique<Label>("MAIN PAGE", Position { 0,0 });
-    title->setMode(true, true, 2); 
+    auto title = make_unique<Label>("MAIN WINDOW", Position{ 0,0 });
+    title->setMode(true, 2);
 
-    auto menu = make_unique<Selector>(Position { 0,4 });
+    auto menu = make_unique<Menu>(Position{ 0,4 });
 
-    auto l_start = make_unique<Label>("Start", Position { 0,0 });
-    auto l_options = make_unique<Label>("Options", Position { 0,3 });
-    auto l_exit = make_unique<Label>("Exit", Position { 0,6 });
+    auto l_start = make_unique<Label>("Start", Position{ 0,0 });
+    auto l_options = make_unique<Label>("Options", Position{ 0,3 });
+    auto l_exit = make_unique<Label>("Exit", Position{ 0,6 });
 
-    menu->addItem(l_start.get(),    [&]() { catalog.push(&start_Page); });
-    menu->addItem(l_options.get(),  [&]() { catalog.push(&options_Page); });
-    menu->addItem(l_exit.get(),     [&]() { exit(0); });
-    Selector* menu_ptr = menu.get();
+    menu->addItem(l_start.get(), [&]() { catalog.push(move(start_Window)); });
+    menu->addItem(l_options.get(), [&]() { catalog.push(move(options_Window)); });
+    menu->addItem(l_exit.get(), [&]() { exit(0); });
+    Menu* menu_ptr = menu.get();
 
-    main_Page.root.addChild(move(title));
-    main_Page.root.addChild(move(menu));
-    main_Page.set_Selector(menu_ptr);
+    main_Window->root.addChild(move(title));
+    main_Window->root.addChild(move(menu));
 
     // ===== START PAGE =====
-    auto start_label = make_unique<Label>("START PAGE", Position { 0,0 });
+    auto start_label = make_unique<Label>("START WINDOW", Position{ 0,0 });
     start_label->setSpecialMode(Color::grn, Label::Vertical);
-    start_label->setMode(true, true, 2);
-    start_Page.root.addChild(move(start_label));
+    start_label->setMode(true, 2);
+    auto start_textBox = make_unique<TextBox>(Position{ 10,7 });
+    start_Window->root.addChild(move(start_label));
+    start_Window->root.addChild(move(start_textBox));
 
     // ===== OPTIONS PAGE =====
-    auto opt_label = make_unique<Label>("OPTIONS PAGE", Position { 0,0 });
-    opt_label->setMode(true, true, 2);
-    options_Page.root.addChild(move(opt_label));
+    auto opt_label = make_unique<Label>("OPTIONS WINDOW", Position{ 0,0 });
+    opt_label->setMode(true, 2);
+    options_Window->root.addChild(move(opt_label));
 
-    catalog.push(&main_Page);
+    catalog.push(move(main_Window));
     catalog.run();
 }
