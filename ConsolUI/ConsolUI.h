ded7b782d7afabb3a3281a4e39132d29172d0fa2
EliamJMC <jozadad2007@gmail.com>
2026-02-03 12:52:58 -0500

    ps

--- ConsolUI/ConsolUI.h
+++ ConsolUI/ConsolUI.h
@@ -4,17 +4,10 @@ struct Position;
 struct Size;
 
 namespace ConsolUI {
-	namespace VisualElements {};
-	namespace Systems {};
-	namespace Managers {};
 	class Controller;
 
-	struct Request {};
+	struct Request;
 	struct Call;
 	struct Event;
-
-	namespace vse = VisualElements;
-	namespace sys = Systems;
-	namespace mng = Managers;
 };
 namespace cui = ConsolUI;
